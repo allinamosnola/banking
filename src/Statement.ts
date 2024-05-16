b5e17b58080f7453e76f8eb0cb7bc804aecfaf56
Alonso <alonso.manilla@gmail.com>
2024-05-16 15:34:18 -0600

    TDD banking

---
+++ src/Statement.ts
@@ -0,0 +1,10 @@
+export class Statement {
+    constructor(private date: Date, private amount: number, private balance: number) {}
+
+    toString(): string {
+        const day = this.date.getDate()
+        const month = this.date.getMonth() + 1
+        return `${day < 10 ? `0${day}`: day}.${month < 10 ? `0${month}` : month}.${this.date.getFullYear()}` +
+        `\t${this.amount > 0 ? '+' : ''}${this.amount}\t\t${this.balance}`
+    }
+}
